c959f527feda3393a6432ce6181538c206018895
yanxu <1424924394@qq.com>
2021-05-09 15:44:29 +0800

    'gshop'

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <router-view />
+     <FooterGuide />
+  </div>
+</template>
+
+<script>
+  import FooterGuide from './components/FooterGuide/FooterGuide'
+  export default {
+    components:{
+      FooterGuide  
+    }
+
+    
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+ #app
+  width 100%
+  height 100%
+  background #f5f5f5
+   
+</style>
